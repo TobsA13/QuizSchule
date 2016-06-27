5e9e046620b42116b00239808cf10532fc2139f8
Tobias Albert <github@tobsa.wtf>
2016-06-27 17:44:54 +0200

    First

---
+++ js/main.js
@@ -0,0 +1,56 @@
+/**
+ * Created by tobias.albert on 27.06.2016.
+ */
+
+
+function generate_html(arr, quest_num){
+    var ans = [];
+    var r = 0;
+    for (var i = 4; i >= 1; i--){
+        var r =  Math.floor((Math.random() * i) + 1);
+            while(typeof ans[r - 1] !== 'undefined'){
+                r++;
+            }
+        ans[r - 1] = arr[i];
+
+    }
+    r--;
+
+    var html = '<div class="quest" id="quest_' + quest_num + '"><div class="question">' + (quest_num + 1) + '. ' + arr[0] + '</div>\n';
+    for (i = 0; i < 4; i++){
+        html += '<div class="answer"><input type="radio" name="quest' + quest_num + '" id="answer_quest' + quest_num +
+            '_' + i + '" data-answer="' + ((r == i) ? 'true' : 'false') + '"><label for="answer_quest' + quest_num + '_' + i + '">' + ans[i] + '</label></div>\n'
+    }
+    $("#game").append(html);
+}
+
+function init_page(){
+    shuffle(questions);
+    for (var i = 0; i < quest_per_page; i++){
+        generate_html(questions[i], i);
+    }
+}
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+function check_answers(){
+    $( ":radio").prop('disabled', true);
+    $( "input[data-answer|='true']").parent().css( "background-color", "rgba(50,255,50, 0.3)" );
+    $( "input[data-answer|='false']").parent().css( "background-color", "rgba(255,0,0,0.3)" );
+    $( "input[data-answer|='true']:checked").parent().parent().children(".question").css( "color", "limegreen" );
+    $( "input[data-answer|='true']:not(:checked)").parent().parent().children(".question").css( "color", "red" );
+    var right_answers = $( "input[data-answer|='true']:checked").length;
+    console.log(right_answers);
+}
+
+$( document ).ready(function() {
+    init_page();
+});
---
+++ js/questions.js
@@ -0,0 +1,47 @@
+/**
+ * Created by tobias.albert on 27.06.2016.
+ */
+
+var questions = [
+    ['test1','ja','nein1','nein2','nein3'],
+    ['test2','ja','nein1','nein2','nein3'],
+    ['test3','ja','nein1','nein2','nein3'],
+    ['test4','ja','nein1','nein2','nein3'],
+    ['test5','ja','nein1','nein2','nein3'],
+    ['test6','ja','nein1','nein2','nein3'],
+    ['test7','ja','nein1','nein2','nein3'],
+    ['test8','ja','nein1','nein2','nein3'],
+    ['test9','ja','nein1','nein2','nein3'],
+    ['test10','ja','nein1','nein2','nein3'],
+    ['test11','ja','nein1','nein2','nein3'],
+    ['test12','ja','nein1','nein2','nein3'],
+    ['test13','ja','nein1','nein2','nein3'],
+    ['test14','ja','nein1','nein2','nein3'],
+    ['test15','ja','nein1','nein2','nein3'],
+    ['test16','ja','nein1','nein2','nein3'],
+    ['test17','ja','nein1','nein2','nein3'],
+    ['test18','ja','nein1','nein2','nein3'],
+    ['test19','ja','nein1','nein2','nein3'],
+    ['test20','ja','nein1','nein2','nein3'],
+    ['test21','ja','nein1','nein2','nein3'],
+    ['test22','ja','nein1','nein2','nein3'],
+    ['test23','ja','nein1','nein2','nein3'],
+    ['test24','ja','nein1','nein2','nein3'],
+    ['test25','ja','nein1','nein2','nein3'],
+    ['test26','ja','nein1','nein2','nein3'],
+    ['test27','ja','nein1','nein2','nein3'],
+    ['test28','ja','nein1','nein2','nein3'],
+    ['test29','ja','nein1','nein2','nein3'],
+    ['test30','ja','nein1','nein2','nein3'],
+    ['test31','ja','nein1','nein2','nein3'],
+    ['test32','ja','nein1','nein2','nein3'],
+    ['test33','ja','nein1','nein2','nein3'],
+    ['test34','ja','nein1','nein2','nein3'],
+    ['test35','ja','nein1','nein2','nein3'],
+    ['test36','ja','nein1','nein2','nein3'],
+    ['test37','ja','nein1','nein2','nein3'],
+    ['test38','ja','nein1','nein2','nein3'],
+    ['test39','ja','nein1','nein2','nein3']
+];
+
+var quest_per_page = 20;
